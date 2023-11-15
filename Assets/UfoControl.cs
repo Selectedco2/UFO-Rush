755f8a076f76cb00a44da3c2c21446d177c2aa92
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2023-11-15 08:33:10 +0100

    la till basic hopp funktion

---
+++ Assets/UfoControl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using Unity.VisualScripting;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+public class UfoControl : MonoBehaviour
+{
+
+ [SerializeField]
+  float jumpForce = 100;
+
+  bool mayJump = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+    if (Input.GetAxisRaw("Jump") > 0 && mayJump == true)
+    {
+      Rigidbody2D rb = GetComponent<Rigidbody2D>();
+      Vector2 jump = Vector2.up * jumpForce;
+
+      rb.AddForce(jump);
+
+      mayJump = false;
+    }
+
+    if (Input.GetAxisRaw("Jump") == 0)
+    {
+      mayJump = true;
+    }
+    
+    }
+    
+  private void OnDrawGizmos()
+  {
+    
+  }
+  
+}
