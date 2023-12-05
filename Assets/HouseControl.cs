50f55844b27f3259b500240358efb605801399f0
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2023-12-05 16:26:05 +0100

    La till en till inmstantiate i housespawn

--- Assets/HouseControl.cs
+++ Assets/HouseControl.cs
@@ -15,7 +15,7 @@ public class HouseControl : MonoBehaviour
     {
         if (UfoControl.canControl == false)
         {
-            
+            HouseSpawner.houseSpeed = 0f;
         }
 
     }
--- Assets/HouseSpawner.cs
+++ Assets/HouseSpawner.cs
@@ -4,7 +4,7 @@ public class HouseSpawner : MonoBehaviour
 {
     public GameObject housePrefab;
     public float spawnInterval = 2f;
-    public float houseSpeed = 5f;
+    static public float houseSpeed = 5f;
 
 
 
@@ -16,17 +16,25 @@ public class HouseSpawner : MonoBehaviour
 
     private void SpawnHouse()
     {
+
         if (UfoControl.canControl == true)
         {
-            GameObject newHouse = Instantiate(housePrefab, transform.position, Quaternion.identity);
+            Vector3 pos = new(transform.position.x, 3 + Random.Range(-Camera.main.orthographicSize, Camera.main.orthographicSize));
+            GameObject newHouse1 = Instantiate(housePrefab, pos, Quaternion.identity);
+            pos.y += 6;
+            GameObject newHouse2 = Instantiate(housePrefab, pos, Quaternion.identity);
 
-            newHouse.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            newHouse1.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            newHouse2.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
 
-            Rigidbody2D houseRb = newHouse.GetComponent<Rigidbody2D>();
+            Rigidbody2D houseRb1 = newHouse1.GetComponent<Rigidbody2D>();
+            Rigidbody2D houseRb2 = newHouse1.GetComponent<Rigidbody2D>();
 
-            houseRb.velocity = new Vector2(-houseSpeed, 0f);
+            houseRb1.velocity = new Vector2(-houseSpeed, 0f);
+            houseRb2.velocity = new Vector2(-houseSpeed, 0f);
 
-            Destroy(newHouse, 5f);
+            Destroy(newHouse1, 5f);
+            Destroy(newHouse2, 5f);
         }
 
         if (UfoControl.canControl == false)
